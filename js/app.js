848f13dcea5f1fc68d325130695463c4f6fad907
Billy Franz <billfranz00@gmail.com>
2015-12-08 19:22:29 -0800

    First Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var stuff = [];
+
+function enterMessage() {
+	var yourName = document.getElementById("name").value,
+		yourMessage = document.getElementById("message").value,
+		h2 = document.createElement("h2"),
+		p = document.createElement("p"),
+		theSelection = document.getElementById("theSelect").value;
+	h2.innerHTML = yourName;
+	p.innerHTML = yourMessage;
+	console.log(theSelection)
+	if (theSelection === 1) {
+		document.getElementById("division").appendChild(h2)
+		document.getElementById("division").appendChild(p)
+	}
+	else {
+		document.body.appendChild(h2)
+		document.body.appendChild(p)
+	}
+}
